3eb4a8894a8f7de88588fd95ea92cece2fced5b8
omlie <lieolemagnus@gmail.com>
2023-12-17 14:36:58 +0100

    update setup scripts

--- setup-new-mac.sh
+++ setup-new-mac.sh
@@ -1,185 +1,21 @@
 #!/bin/bash
+set -e
 
-echo "Creating an SSH key for you..."
-ssh-keygen -t rsa
+scripts/install_homebrew.sh
+scripts/install_omz.sh
 
-# Check for Homebrew,
-# Install if we don't have it
-if test ! $(which brew); then
-  echo "Installing homebrew..."
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
+# echo "Git config"
 
-
-echo "Installing xcode-stuff"
-xcode-select --install
-
-# Update homebrew recipes
-echo "Updating homebrew..."
-brew update
-
-echo "Installing Git..."
-brew install git
-
-echo "Git config"
-
-git config --global user.name "Ole Magnus Lie"
-git config --global user.email lieolemagnus@gmail.com
-
-
-echo "Installing other brew stuff..."
-brew install wget
-brew install node
-brew install deno
-brew install koekeishiya/formulae/yabai
-
-
-brew tap homebrew/cask-fonts
-
-
-echo "Cleaning up brew"
-brew cleanup
-
-echo "Installing homebrew cask"
-brew install caskroom/cask/brew-cask
+#git config --global user.name "Ole Magnus Lie"
+#git config --global user.email lieolemagnus@gmail.com
 
 echo "Symlinking dotfiles"
-cd ~/.dotfiles
-sh symlink-dotfiles
-
-#Install Zsh & Oh My Zsh
-echo "Installing Oh My ZSH..."
-curl -L http://install.ohmyz.sh | sh
+./symlink-dotfiles.sh
 
 echo "Setting ZSH as shell..."
 chsh -s /bin/zsh
 source ~/.zshrc
 
-# Apps
-apps=(
-  1password
-  1password-cli
-  font-fira-code
-  iterm2
-  raycast
-  arc
-  spotify
-  discord
-  telegram
-  slack
-  visual-studio-code
-  iterm2
-  fig
-)
-
-# Install apps to /Applications
-# Default is: /Users/$user/Applications
-echo "installing apps with Cask..."
-brew install --cask --appdir="/Applications" ${apps[@]}
-
-brew cleanup
-
-echo "Setting some Mac settings..."
-
-#"Disabling system-wide resume"
-defaults write NSGlobalDomain NSQuitAlwaysKeepsWindows -bool false
-
-#"Disabling automatic termination of inactive apps"
-defaults write NSGlobalDomain NSDisableAutomaticTermination -bool true
-
-#"Allow text selection in Quick Look"
-defaults write com.apple.finder QLEnableTextSelection -bool TRUE
-
-#"Expanding the save panel by default"
-defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
-defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
-defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
-
-#"Automatically quit printer app once the print jobs complete"
-defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
-
-#"Saving to disk (not to iCloud) by default"
-defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
-
-#"Check for software updates daily, not just once per week"
-defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
-
-#"Disable smart quotes and smart dashes as they are annoying when typing code"
-defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
-defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
-
-#"Enabling full keyboard access for all controls (e.g. enable Tab in modal dialogs)"
-defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
-
-#"Setting trackpad & mouse speed to a reasonable number"
-defaults write -g com.apple.trackpad.scaling 1
-defaults write -g com.apple.mouse.scaling 2
-
-#"Enabling subpixel font rendering on non-Apple LCDs"
-defaults write NSGlobalDomain AppleFontSmoothing -int 2
-
-#"Showing icons for hard drives, servers, and removable media on the desktop"
-defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
-
-#"Showing all filename extensions in Finder by default"
-defaults write NSGlobalDomain AppleShowAllExtensions -bool true
-
-#"Disabling the warning when changing a file extension"
-defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
-
-#"Use column view in all Finder windows by default"
-defaults write com.apple.finder FXPreferredViewStyle Clmv
-
-#"Avoiding the creation of .DS_Store files on network volumes"
-defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
-
-#"Enabling snap-to-grid for icons on the desktop and in other icon views"
-/usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
-/usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
-/usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
-
-#"Setting the icon size of Dock items to 36 pixels for optimal size/screen-realestate"
-defaults write com.apple.dock tilesize -int 36
-
-#"Speeding up Mission Control animations and grouping windows by application"
-defaults write com.apple.dock expose-animation-duration -float 0.1
-defaults write com.apple.dock "expose-group-by-app" -bool true
-
-#"Setting Dock to auto-hide and removing the auto-hiding delay"
-defaults write com.apple.dock autohide -bool true
-defaults write com.apple.dock autohide-delay -float 0
-defaults write com.apple.dock autohide-time-modifier -float 0
-
-#"Preventing Time Machine from prompting to use new hard drives as backup volume"
-defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
-
-#"Disable the sudden motion sensor as its not useful for SSDs"
-sudo pmset -a sms 0
-
-#"Speeding up wake from sleep to 24 hours from an hour"
-# http://www.cultofmac.com/221392/quick-hack-speeds-up-retina-macbooks-wake-from-sleep-os-x-tips/
-sudo pmset -a standbydelay 86400
-
-#"Setting screenshot format to PNG"
-defaults write com.apple.screencapture type -string "png"
-
-#"Enabling the Develop menu and the Web Inspector in Safari"
-defaults write com.apple.Safari IncludeDevelopMenu -bool true
-defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
-defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
-
-#"Adding a context menu item for showing the Web Inspector in web views"
-defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
-
-#"Disable 'natural' (Lion-style) scrolling"
-defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
-
-# Don’t automatically rearrange Spaces based on most recent use
-defaults write com.apple.dock mru-spaces -bool false
-
-
-
-killall Finder
-
+./scripts/set_mac_settings.sh
 
 echo "Done!"
--- symlink-dotfiles.sh
+++ symlink-dotfiles.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 stow zsh
+stow sketchybar
+stow yabai
